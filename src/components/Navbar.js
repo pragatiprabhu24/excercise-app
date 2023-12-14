3f51a3666f02c85c4e3a7f30fc443d02980fc5ed
pragatiprabhu24 <prabhupragati5@gmail.com>
2023-12-14 17:22:09 +0530

    commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,7 +8,7 @@ import logo from '../assets/images/Logo.png'
 
 const menu = [
     { name: 'Home', href: '/' },
-    { name: 'Excercises', href: '/excercises' }
+ 
 ]
 
 
@@ -23,7 +23,7 @@ export default function Navbar() {
         <header className="bg-white">
             <nav className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8" aria-label="Global">
                 <div className="flex lg:flex-1">
-                    <a href="#" className="-m-1.5 p-1.5">
+                    <a href="/" className="-m-1.5 p-1.5">
                         <span className="sr-only">Your Company</span>
                         <img className="h-8 w-auto" src={logo} alt="" />
                     </a>
